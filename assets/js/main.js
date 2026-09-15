d3055143ab70621e786aaf379a0f1153232106bd
Santoso Pizza Delivery <santosopizzadelivery@gmail.com>
2026-09-15 17:58:38 +0700

    Update nomor WA & link Pardi

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ if ('serviceWorker' in navigator) {
   var form = document.getElementById('contact-form');
   if (!form) return;
 
-  var WA_NUMBER = '6281200000000'; // TODO: ganti dengan nomor WhatsApp bisnis SantoSoft
+  var WA_NUMBER = '6281212551846'; // TODO: ganti dengan nomor WhatsApp bisnis SantoSoft
 
   function encode(data) {
     return Object.keys(data)
